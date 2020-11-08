7a7f9d1265d55419eabc79df52ed0cb3a66acbc2
lizhe <364756388@qq.com>
2020-11-08 20:47:13 +0800

    第一次调试

---
+++ routes/test.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var URL = require('url');
+//加载mysql模块
+var mysql = require('mysql');
+//创建连接
+var connection = mysql.createConnection({
+  host: '192.168.31.73',
+  user: 'root',
+  password: '',
+  database: 'caterpie'
+});
+//执行创建连接 
+connection.connect();
+//SQL语句
+var sql = 'SELECT * FROM yushuwu where yid = ?';
+
+router.get('/:yid/', function (req, res, next) {
+  //解析请求参数
+  var params = req.params.yid;
+
+  //查
+  connection.query(sql, params, function (err, result) {
+    if (err) {
+      console.log('[SELECT ERROR] - ', err.message);
+      return;
+    }
+    console.log(params.id);
+
+    //把搜索值输出
+    res.render('test', {result});
+
+  });
+});
+
+module.exports = router;
